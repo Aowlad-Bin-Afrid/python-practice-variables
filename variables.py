838b23b8b06d11e62ee94882755d1606e3ab1b68
Aowlad Bin Afrid <gitcxcc@gmail.com>
2025-10-27 10:25:56 +0600

    variables.py

--- variables.py
+++ variables.py
@@ -1,28 +1,32 @@
-#variable
+# Basic variable examples
+# Created by Afrid
+# Date: 2025-10-27
+# Description: Basic examples of Python variables
+
+# simple variable
 name = "Afrid"
 print(name)
 
-#variable
+# multiple variables
 x = "age"
 y = 18
-print(x,y)
+print(x, y)
 
-# variable 
+# variables with f-string
 name = "Afrid"
-age  = 18
+age = 18
 city = "Dhaka"
-print(f"Hi my name is {name}.Im {age} years old. I live in {city} city.")
+print(f"Hi, my name is {name}. I'm {age} years old. I live in {city} city.")
 
-#variable
+# updating variable value
 name = "Afrid"
-age  = 18
-print(f"my name is {name}. im {age} years old.")
-age = age +1
-print(f"next year i will be {age} .")
+age = 18
+print(f"My name is {name}. I'm {age} years old.")
+age = age + 1   # update variable
+print(f"Next year I will be {age} years old.")
 
-# input field and variable
+# taking input from user
 name = input("Enter your name: ")
-age  = int(input("Enter your age: "))
-age += 1
-print(f"{name} next year you will be {age} years old.")
-
+age = int(input("Enter your age: "))
+age += 1   # update with +1
+print(f"{name}, next year you will be {age} years old.")
